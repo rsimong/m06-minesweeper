557e12728d05f208827bc6c75c869f7da9f064af
Ricard SG <rsimon@mariabarcelona.com>
2020-02-08 15:35:38 +0100

    Install NPM Packages: Babel, Webpack and ESLint

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+module.exports = {
+    entry: ['babel-polyfill', './src/js/index.js'],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        }),
+        // new BundleAnalyzerPlugin({
+        //     analyzerMode: 'static', //para que lo haga sólo al momento de hacer el build
+        //     openAnalyzer: true, //para que nos muestre el resultado inmediatamente
+        // }),
+        new MiniCssExtractPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                enforce: 'pre',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'eslint-loader',
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            }
+        ]
+    }
+}
